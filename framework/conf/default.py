4b9cbfa1cecd5b3616e0a91ac124a755dd3c2ce3
upblue <18922169626@189.cn>
2018-08-28 19:01:31 +0800

    update database

--- framework/conf/default.py
+++ framework/conf/default.py
@@ -22,8 +22,8 @@ from django.conf.global_settings import *
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID =  'bluework1'
-APP_TOKEN = '0b348dae-5f44-408d-a68a-056e3357709f'
+APP_ID =  'fblue'
+APP_TOKEN = '98d2124d-5304-42f3-a01c-a980a9b00051'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.blueking.com'
 
--- framework/conf/settings_testing.py
+++ framework/conf/settings_testing.py
@@ -11,7 +11,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'fblue',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'fblueprod',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'OIraQ3SJ_x',                        # 你的数据库password
         'HOST': '172.50.19.22',                   		   # 数据库HOST
